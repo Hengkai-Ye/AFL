9bc03990fd62e902be23682a353639b925e1a4b0
hengkai-ye <826827717@qq.com>
2021-07-05 20:44:34 -0700

    week5

--- afl-fuzz.c
+++ afl-fuzz.c
@@ -1015,6 +1015,107 @@ static inline u8 has_new_bits(u8* virgin_map) {
 
 }
 
+static inline u8 has_new_bits_2(u8* virgin_map) {
+
+#ifdef WORD_SIZE_64
+
+  u64* current = (u64*)trace_bits;
+  u64* virgin  = (u64*)virgin_map;
+
+  u32  i = (MAP_SIZE >> 3);
+
+#else
+
+  u32* current = (u32*)trace_bits;
+  u32* virgin  = (u32*)virgin_map;
+
+  u32  i = (MAP_SIZE >> 2);
+
+#endif /* ^WORD_SIZE_64 */
+
+  u8   ret = 0;
+  while (i--) {
+
+    /* Optimize for (*current & *virgin) == 0 - i.e., no bits in current bitmap
+       that have not been already cleared from the virgin map - since this will
+       almost always be the case. */
+
+    if (unlikely(*current) && unlikely(*current & *virgin)) {
+
+      if(i > 0) {
+        u8* cur = (u8*)current;
+        u8* vir = (u8*)virgin;
+
+        /* Looks like we have not found any new bytes yet; see if any non-zero
+           bytes in current[] are pristine in virgin[]. */
+
+#ifdef WORD_SIZE_64
+        if ((cur[0] && vir[0] == 0xff) || (cur[1] && vir[1] == 0xff) ||
+            (cur[2] && vir[2] == 0xff) || (cur[3] && vir[3] == 0xff) ||
+            (cur[4] && vir[4] == 0xff) || (cur[5] && vir[5] == 0xff) ||
+            (cur[6] && vir[6] == 0xff) || (cur[7] && vir[7] == 0xff)) {
+          if(cur[0] && vir[0] == 0xff){
+            printf("%d:NB\n", 65536 - i*8 - 8);
+            ret = 2;
+          }
+          else if(cur[1] && vir[1] == 0xff){
+            printf("%d:NB\n", 65536 - i*8 + 1 - 8);
+            ret = 2;
+          }
+          else if(cur[2] && vir[2] == 0xff){
+            printf("%d:NB\n", 65536 - i*8 + 2 - 8);
+            ret = 2;
+          }
+          else if(cur[3] && vir[3] == 0xff){
+            printf("%d:NB\n", 65536 - i*8 + 3 - 8);
+            ret = 2;
+          }
+          else if(cur[4] && vir[4] == 0xff){
+            printf("%d:NB\n", 65536 - i*8 + 4 - 8);
+            ret = 2;
+          }
+          else if(cur[5] && vir[5] == 0xff){
+            printf("%d:NB\n", 65536 - i*8 + 5 - 8);
+            ret = 2;
+          }
+          else if(cur[6] && vir[6] == 0xff){
+            printf("%d:NB\n", 65536 - i*8 + 6 - 8);
+            ret = 2;
+          }
+          else if(cur[7] && vir[7] == 0xff){
+            printf("%d:NB\n", 65536 - i*8 + 7 - 8);
+            ret = 2;
+          }              
+        }
+        else{
+          //printf("hit-count\n");
+          ret = 0; 
+        }
+        //else ret = 1;
+#else
+
+        if ((cur[0] && vir[0] == 0xff) || (cur[1] && vir[1] == 0xff) ||
+            (cur[2] && vir[2] == 0xff) || (cur[3] && vir[3] == 0xff)) ret = 2;
+        else ret = 1;
+
+#endif /* ^WORD_SIZE_64 */
+
+      }
+
+      *virgin &= ~*current;
+
+    }
+
+    current++;
+    virgin++;
+
+  }
+
+  if (ret && virgin_map == virgin_bits) bitmap_changed = 1;
+
+  return ret;
+
+}
 
 /* Count the number of bits set in the provided bitmap. Used for the status
    screen several times every second, does not have to be fast. */
@@ -2762,7 +2863,157 @@ abort_calibration:
 
 }
 
+static u8 calibrate_case_2(char** argv, struct queue_entry* q, u8* use_mem,
+                         u32 handicap, u8 from_queue) {
 
+  static u8 first_trace[MAP_SIZE];
+
+  u8  fault = 0, new_bits = 0, var_detected = 0, hnb = 0,
+      first_run = (q->exec_cksum == 0);
+
+  u64 start_us, stop_us;
+
+  s32 old_sc = stage_cur, old_sm = stage_max;
+  u32 use_tmout = exec_tmout;
+  u8* old_sn = stage_name;
+
+  /* Be a bit more generous about timeouts when resuming sessions, or when
+     trying to calibrate already-added finds. This helps avoid trouble due
+     to intermittent latency. */
+
+  if (!from_queue || resuming_fuzz)
+    use_tmout = MAX(exec_tmout + CAL_TMOUT_ADD,
+                    exec_tmout * CAL_TMOUT_PERC / 100);
+
+  q->cal_failed++;
+
+  stage_name = "calibration";
+  stage_max  = fast_cal ? 3 : CAL_CYCLES;
+
+  /* Make sure the forkserver is up before we do anything, and let's not
+     count its spin-up time toward binary calibration. */
+
+  if (dumb_mode != 1 && !no_forkserver && !forksrv_pid)
+    init_forkserver(argv);
+
+  if (q->exec_cksum) {
+
+    memcpy(first_trace, trace_bits, MAP_SIZE);
+    hnb = has_new_bits_2(virgin_bits);
+    if (hnb > new_bits) new_bits = hnb;
+
+  }
+
+  start_us = get_cur_time_us();
+
+  for (stage_cur = 0; stage_cur < stage_max; stage_cur++) {
+
+    u32 cksum;
+
+    if (!first_run && !(stage_cur % stats_update_freq)) show_stats();
+
+    write_to_testcase(use_mem, q->len);
+
+    fault = run_target(argv, use_tmout);
+
+    /* stop_soon is set by the handler for Ctrl+C. When it's pressed,
+       we want to bail out quickly. */
+
+    if (stop_soon || fault != crash_mode) goto abort_calibration;
+
+    if (!dumb_mode && !stage_cur && !count_bytes(trace_bits)) {
+      fault = FAULT_NOINST;
+      goto abort_calibration;
+    }
+
+    cksum = hash32(trace_bits, MAP_SIZE, HASH_CONST);
+
+    if (q->exec_cksum != cksum) {
+
+      hnb = has_new_bits_2(virgin_bits);
+      if (hnb > new_bits) new_bits = hnb;
+
+      if (q->exec_cksum) {
+
+        u32 i;
+
+        for (i = 0; i < MAP_SIZE; i++) {
+
+          if (!var_bytes[i] && first_trace[i] != trace_bits[i]) {
+
+            var_bytes[i] = 1;
+            stage_max    = CAL_CYCLES_LONG;
+
+          }
+
+        }
+
+        var_detected = 1;
+
+      } else {
+
+        q->exec_cksum = cksum;
+        memcpy(first_trace, trace_bits, MAP_SIZE);
+
+      }
+
+    }
+
+  }
+
+  stop_us = get_cur_time_us();
+
+  total_cal_us     += stop_us - start_us;
+  total_cal_cycles += stage_max;
+
+  /* OK, let's collect some stats about the performance of this test case.
+     This is used for fuzzing air time calculations in calculate_score(). */
+
+  q->exec_us     = (stop_us - start_us) / stage_max;
+  q->bitmap_size = count_bytes(trace_bits);
+  q->handicap    = handicap;
+  q->cal_failed  = 0;
+
+  total_bitmap_size += q->bitmap_size;
+  total_bitmap_entries++;
+
+  update_bitmap_score(q);
+
+  /* If this case didn't result in new output from the instrumentation, tell
+     parent. This is a non-critical problem, but something to warn the user
+     about. */
+
+  if (!dumb_mode && first_run && !fault && !new_bits) fault = FAULT_NOBITS;
+
+abort_calibration:
+
+  if (new_bits == 2 && !q->has_new_cov) {
+    q->has_new_cov = 1;
+    queued_with_cov++;
+  }
+
+  /* Mark variable paths. */
+
+  if (var_detected) {
+
+    var_byte_count = count_bytes(var_bytes);
+
+    if (!q->var_behavior) {
+      mark_as_variable(q);
+      queued_variable++;
+    }
+
+  }
+
+  stage_name = old_sn;
+  stage_cur  = old_sc;
+  stage_max  = old_sm;
+
+  if (!first_run) show_stats();
+
+  return fault;
+
+}
 /* Examine map coverage. Called once, for first test case. */
 
 static void check_map_coverage(void) {
@@ -2782,7 +3033,7 @@ static void check_map_coverage(void) {
 /* Perform dry run of all test cases to confirm that the app is working as
    expected. This is done only for the initial inputs, and only once. */
 
-static void perform_dry_run(char** argv) {
+static void perform_dry_run(char** argv, int dry_mode) {
   struct queue_entry* q = queue;
   u32 cal_failures = 0;
   u32 chain_index = 0;
@@ -2806,8 +3057,11 @@ static void perform_dry_run(char** argv) {
       FATAL("Short read from '%s'", q->fname);
 
     close(fd);
-
-    res = calibrate_case(argv, q, use_mem, 0, 1);
+    if(dry_mode){
+      res = calibrate_case_2(argv, q, use_mem, 0, 1);
+    }else{
+      res = calibrate_case(argv, q, use_mem, 0, 1);
+    }
     ck_free(use_mem);
 
     if (stop_soon) return;
@@ -8107,9 +8361,11 @@ int main(int argc, char** argv) {
   else
     use_argv = argv + optind;
 
-  perform_dry_run(use_argv);
-  if (dry_mode)
+  if (dry_mode){
+    perform_dry_run(use_argv, dry_mode);
     exit(0);
+  }
+  perform_dry_run(use_argv, dry_mode);
   cull_queue();
 
   show_init_stats();
